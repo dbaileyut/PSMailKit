4746182d1fe73409323dabc109f209e59861a968
Doug Bailey <dbailey@utexas.edu>
2023-04-21 18:25:27 -0500

    Attempted to publish but name is taken

--- build/Publish-ToGallery.ps1
+++ build/Publish-ToGallery.ps1
@@ -1,2 +1,14 @@
 $ModuleRoot = (Get-Item (Join-Path -Path $PSScriptRoot '..')).FullName
-Invoke-ScriptAnalyzer -Recurse -Severity Warning -Path $ModuleRoot
+$WarningsAndErrors = Invoke-ScriptAnalyzer -Recurse -Severity Warning -Path $ModuleRoot
+if ($WarningsAndErrors) {
+    Write-Error -Message "Script Analyzer found $($WarningsAndErrors.Count) warnings and errors. See output for details."
+    $WarningsAndErrors | Format-List -Force
+    return
+}
+
+if (-not $NugetApiKey) {
+    $Credential = Get-Credential -Message "Enter your NuGet API key" -UserName "NuGet API Key"
+    $NugetApiKey = $Credential.GetNetworkCredential().Password
+}
+# Doh PSMailKit name is already taken on the gallery.  Need to rename the module. Asked the gallery owner to let me take it.
+Publish-Module -Path $ModuleRoot -NuGetApiKey $NugetApiKey -Repository PSGallery -Verbose
